dbfebf26cf45ba25e54359d28609c439b943a3e3
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-11-09 09:27:42 +0100

    tried to display the lorem ipsum but can't see anything on my page. I think because I had some difficulties by the setup of the project. The default naming in some of my files is still symf-mvc/symf-mvc. It's hard for me I can't see the results on my screen

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+//todo do you need to add your html template here
 
 class LearningController extends AbstractController
 {
@@ -17,4 +18,26 @@ class LearningController extends AbstractController
             'controller_name' => 'LearningController',
         ]);
     }
+    /**
+     * @Route("/learning", name="learning")
+     */
+    public function aboutme(): Response
+    {
+        return $this->render('learning/aboutme.html.twig', [
+            'controller_name' => 'LearningController',
+        ]);
+    }
 }
+
+
+/*{
+
+    public function index(): Response
+{
+    return $this->render('learning/index.html.twig', [
+        'controller_name' => 'LearningController',
+    ]);
+}
+}*/
+
+
